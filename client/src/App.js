7a29abac681b99a542e287aa7dcc8e3c4e32fafa
Natalie Spencer <gnataliespen@gmail.com>
2019-11-09 16:27:39 -0600

    basic header

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Splash from "./pages/Splash";
+import Header from "./components/Header";
+import "./css/Style.css";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Splash} />
