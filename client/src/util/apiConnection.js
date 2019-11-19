0702d33a290665b43696c0d64f928db0bdf2f870
Natalie Spencer <gnataliespen@gmail.com>
2019-11-19 06:31:57 -0600

    server only

--- client/src/util/apiConnection.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-//Set base url to server
-let url =
-  process.env.NODE_ENV === "production" ? null : "http://localhost:4000/";
-
-export default axios.create({
-  baseURL: url
-});
